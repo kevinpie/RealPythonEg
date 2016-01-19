2571e088d65bafb6c8ccfd0b72fdc3151218cd3c
Kevin Koshy <kevin@logicmash.com>
2016-01-19 18:07:17 +0530

    3rd commit

--- dictionary_eg.py
+++ dictionary_eg.py
@@ -1,3 +1,21 @@
-phone_book = {"Jenny":"123-34","Kevin":"233-098"}
-print phone_book
-print phone_book["Kevin"]
+import random
+capitals_dict = {
+'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta',
+}
+
+x = random.choice(capitals_dict.keys())
+y = capitals_dict[x]
+
+while True:
+        z = raw_input("What is the captital of " + x)
+        if z == y:
+            print "Correct"
+            exit()
+
+        elif z == "exit":
+            exit()
+
+        else:
+            print "Keep Going"
+
+
