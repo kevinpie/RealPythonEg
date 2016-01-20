4540751952da90abab5ec27e732d63d47acd114f
Kevin Koshy <kevin@logicmash.com>
2016-01-20 20:56:30 +0530

    1st Commit

---
+++ file_io_ver2.py
@@ -0,0 +1,7 @@
+my_input_file = open("hello.txt", 'r')
+print "Line 0 (first line):", my_input_file.readline()
+
+my_input_file.seek(0)
+print "Line 0 again: ", my_input_file.readline()
+print "Line 1: ", my_input_file.readline()
+
